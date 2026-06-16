f5337ae1ed38f20148dc0b1e68152ac3cbaca9b3
TomNaber <tomnaber12@gmail.com>
2026-06-16 17:57:41 +0200

    Formatting changes and estimate shared age rate gam + quadratic variance including covariance with bootstrapping

--- scripts/bioage_package/hd_calc.R
+++ scripts/bioage_package/hd_calc.R
@@ -22,10 +22,10 @@
 #' @import dplyr
 
 hd_calc <- function(data, reference, biomarkers) {
- ref <- as.matrix(reference[, ..biomarkers])
+  ref <- as.matrix(reference[, ..biomarkers])
   dat <- as.matrix(data[, ..biomarkers])
 
-  #tandardize variables by mean and sd of reference population
+  # tandardize variables by mean and sd of reference population
   for (j in 1:ncol(dat)) {
     dat[, j] <- (dat[, j] - mean(ref[, j], na.rm = TRUE)) / sd(ref[, j], na.rm = TRUE)
   }
