0c24f9df3756d73729a3bf98634def99f7014609
TomNaber <tomnaber12@gmail.com>
2026-09-24 12:39:37 +0200

    Return unscaled HD additionally

--- scripts/bioage_package/hd_calc.R
+++ scripts/bioage_package/hd_calc.R
@@ -47,11 +47,12 @@ hd_calc <- function(data, reference, biomarkers) {
     select(SEQN, all_of(biomarkers)) %>%
     na.omit()
 
+  dat$hd_raw <- hd
   dat$hd <- hd / sd(hd)
   dat$hd_log <- log(hd) / sd(log(hd))
   nobs <- sum(!is.na(dat$hd))
 
-  dat <- left_join(data, dat[, c("SEQN", "hd", "hd_log")], by = "SEQN")
+  dat <- left_join(data, dat[, c("SEQN", "hd_raw", "hd", "hd_log")], by = "SEQN")
   fit <- list(mcov = means, cov_mat = cv_mat, nobs = nobs)
   hd <- list(data = as.data.frame(dat), fit = fit)
 
